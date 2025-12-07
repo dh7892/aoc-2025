35c8887430bc7d2d1df3ef7b19cd07e317f4f541
Dave <dave@dhills.net>
2025-12-07 11:36:45 +0000

    More fixes for 12 days

--- src/template/day.rs
+++ src/template/day.rs
@@ -8,7 +8,7 @@ use chrono::{Datelike, FixedOffset, Utc};
 #[cfg(feature = "today")]
 const SERVER_UTC_OFFSET: i32 = -5;
 
-/// A valid day number of advent (i.e. an integer in range 1 to 25).
+/// A valid day number of advent (i.e. an integer in range 1 to 12).
 ///
 /// # Display
 /// This value displays as a two digit number.
@@ -25,7 +25,7 @@ impl Day {
     /// Creates a [`Day`] from the provided value if it's in the valid range,
     /// returns [`None`] otherwise.
     pub const fn new(day: u8) -> Option<Self> {
-        if day == 0 || day > 25 {
+        if day == 0 || day > 12 {
             return None;
         }
         Some(Self(day))
@@ -39,11 +39,11 @@ impl Day {
 
 #[cfg(feature = "today")]
 impl Day {
-    /// Returns the current day if it's between the 1st and the 25th of december, `None` otherwise.
+    /// Returns the current day if it's between the 1st and the 12 of december, `None` otherwise.
     pub fn today() -> Option<Self> {
         let offset = FixedOffset::east_opt(SERVER_UTC_OFFSET * 3600)?;
         let today = Utc::now().with_timezone(&offset);
-        if today.month() == 12 && today.day() <= 25 {
+        if today.month() == 12 && today.day() <= 12 {
             Self::new(u8::try_from(today.day()).ok()?)
         } else {
             None
@@ -88,18 +88,18 @@ impl Error for DayFromStrError {}
 
 impl Display for DayFromStrError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("expecting a day number between 1 and 25")
+        f.write_str("expecting a day number between 1 and 12")
     }
 }
 
 /* -------------------------------------------------------------------------- */
 
-/// An iterator that yields every day of advent from the 1st to the 25th.
+/// An iterator that yields every day of advent from the 1st to the 12.
 pub fn all_days() -> AllDays {
     AllDays::new()
 }
 
-/// An iterator that yields every day of advent from the 1st to the 25th.
+/// An iterator that yields every day of advent from the 1st to the 12.
 pub struct AllDays {
     current: u8,
 }
@@ -135,7 +135,7 @@ macro_rules! day {
     ($day:expr) => {
         const {
             $crate::template::Day::new($day)
-                .expect("invalid day number, expecting a value between 1 and 25")
+                .expect("invalid day number, expecting a value between 1 and 12")
         }
     };
 }
@@ -162,19 +162,6 @@ mod tests {
         assert_eq!(iter.next(), Some(Day(10)));
         assert_eq!(iter.next(), Some(Day(11)));
         assert_eq!(iter.next(), Some(Day(12)));
-        assert_eq!(iter.next(), Some(Day(13)));
-        assert_eq!(iter.next(), Some(Day(14)));
-        assert_eq!(iter.next(), Some(Day(15)));
-        assert_eq!(iter.next(), Some(Day(16)));
-        assert_eq!(iter.next(), Some(Day(17)));
-        assert_eq!(iter.next(), Some(Day(18)));
-        assert_eq!(iter.next(), Some(Day(19)));
-        assert_eq!(iter.next(), Some(Day(20)));
-        assert_eq!(iter.next(), Some(Day(21)));
-        assert_eq!(iter.next(), Some(Day(22)));
-        assert_eq!(iter.next(), Some(Day(23)));
-        assert_eq!(iter.next(), Some(Day(24)));
-        assert_eq!(iter.next(), Some(Day(25)));
         assert_eq!(iter.next(), None);
     }
 }
